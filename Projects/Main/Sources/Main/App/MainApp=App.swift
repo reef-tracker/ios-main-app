a71fa7ebf5140f4d720ea1c31be080e77f488af0
Gor Gyolchanyan <gor.gyolchanyan@icloud.com>
2024-03-10 13:15:12 -0400

    Add `Main.xcodeproj` with **main-app** target.

---
+++ Projects/Main/Sources/Main/App/MainApp=App.swift
@@ -0,0 +1,22 @@
+//
+// Introductory information is in the `README.md` file at the repository root containing this file.
+// Copyright information is in the `LICENSE.txt` file at the repository root containing this file.
+//
+
+import SwiftUI
+
+@main
+internal struct MainApp: App {
+
+    // MARK: App
+
+    internal init() {
+        /* This routine is intentionally left blank. */
+    }
+
+    internal var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ Projects/Main/Sources/Main/View/MainView=View.swift
@@ -0,0 +1,32 @@
+//
+// Introductory information is in the `README.md` file at the repository root containing this file.
+// Copyright information is in the `LICENSE.txt` file at the repository root containing this file.
+//
+
+import SwiftUI
+
+#Preview {
+    MainView()
+}
+
+internal struct MainView {
+
+    // MARK: MainView - Initialization
+
+    internal init() {
+        /* This scope is intentionally left blank. */
+    }
+
+    // MARK: MainView - Representation
+
+    /* <#...#> */
+}
+
+extension MainView: View {
+
+    // MARK: View - Body
+
+    internal var body: some View {
+        EmptyView()
+    }
+}
